b8d7d19c8b896cf54a72b946c6f7e574a6460427
John Zank <john.k.zank@gmail.com>
2020-02-04 05:31:53 -0500

    feat(App): Added constructor and state to App class.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
       err => console.log(err)
     );
+  }
 
-    return <div>Lattitude: </div>;
+  render() {
+    return <div>Lattitude: {this.state.lat} </div>;
   }
 }
 
